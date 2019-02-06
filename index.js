22574fb88b1b5fbe0b255cdbf7399aa7edbb6e7d
Elizabeth Thys <elizabeth.thys@gmail.com>
2019-02-06 21:07:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -58,8 +58,7 @@ function createRock(x) {
   rock.className = 'rock'
   rock.style.left = `${x}px`
   // Hmmm, why would we have used `var` here?
-  var top = 0;
-  rock.style.top = top
+  var top = rock.style.top = 0;
   /**
    * Now that we have a rock, we'll need to append
    * it to GAME and move it downwards.
