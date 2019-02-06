a2e2c1c63106058aeedd4f0bcfce7be28f1c892e
Elizabeth Thys <elizabeth.thys@gmail.com>
2019-02-06 21:06:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -111,7 +111,7 @@ function createRock(x) {
  */
 function endGame() {
 clearInterval(gameInterval);
-ROCKS.forEach((rock) => rock.remove());
+ROCKS.forEach(function(rock) {rock.remove()});
 document.removeEventListener('keydown', moveDodger);
 return alert ("YOU LOSE!");
 }
